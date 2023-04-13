bd9ec228673ea468e2b2fc58a78aca2e19fe1820
Hany Juliana <64250079+hanysitio98@users.noreply.github.com>
2023-04-13 09:50:59 +0700

    Initial commit

---
+++ src/main/java/id/co/bca/spring/FunTravelHoliday/FunTravelHoliday/FunTravelHolidayApplication.java
@@ -0,0 +1,13 @@
+package id.co.bca.spring.FunTravelHoliday.FunTravelHoliday;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FunTravelHolidayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FunTravelHolidayApplication.class, args);
+	}
+
+}
